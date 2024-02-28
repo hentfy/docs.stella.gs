094f1a8b4e5cdef1a914834a19ff7a6403f9a4ea
hentfy <159498974+hentfy@users.noreply.github.com>
2024-02-28 16:16:37 +0200

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,16 @@
+--@session start
+local connect_api = api_key
+local stella_fetch
+
+if connect_api then
+    stella_fetch = connect_api()
+else
+    stella_fetch = { 
+        username = '', 
+        version = '', 
+        discord = '' ,
+        api = "L41x0a3D6x2aPKFlUa"
+    }
+end
+
+print("Welcome back,", username)
